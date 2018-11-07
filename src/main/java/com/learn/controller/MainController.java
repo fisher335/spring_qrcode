0d0b86bc801eb37f5958cefd946bba6125fea8e8
fengshaomin <fengshaomin@bjsasc.com>
2018-11-07 14:47:37 +0800

    加入了hutool工具包

--- src/main/java/com/learn/controller/MainController.java
+++ src/main/java/com/learn/controller/MainController.java
@@ -1,21 +1,29 @@
 package com.learn.controller;
 
+import cn.hutool.Hutool;
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.file.LineSeparator;
+import cn.hutool.core.util.NetUtil;
+import cn.hutool.http.HttpUtil;
 import com.learn.util.FileUpload;
 import com.learn.util.Qrcode;
+import org.apache.bcel.generic.NEW;
+import org.apache.bcel.generic.RETURN;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.print.attribute.standard.PrinterLocation;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -30,9 +38,12 @@ public class MainController {
     @Autowired
     private FileUpload fileupload;
 
+    @Value("${web.file-path}")
+    private String upload_path;
+
     private Logger logger = Logger.getLogger(getClass().toString());
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "")
     public String hello() {
         return "index";
     }
@@ -42,7 +53,7 @@ public class MainController {
         return "index";
     }
 
-    @RequestMapping(value = "/qrcode")
+    @GetMapping(value = "/qrcode")
     public String Qrcode() {
         return "index";
     }
@@ -60,7 +71,7 @@ public class MainController {
     }
 
     @RequestMapping(value = "/download/{file_name}")
-    public void download_img(@PathVariable String file_name, HttpServletResponse res) throws UnsupportedEncodingException {
+    public void download_img(@PathVariable("file_name") String file_name, HttpServletResponse res) throws UnsupportedEncodingException {
         qrcode.download(res, file_name);
     }
 
@@ -70,13 +81,15 @@ public class MainController {
     }
 
     @PostMapping(value = "/upload")
-    public String upload_file_post(@RequestParam(value = "file") MultipartFile file) throws IOException {
+    public String upload_file_post(@RequestParam("file") MultipartFile file) throws IOException {
 
         String file_name = file.getOriginalFilename();
         File tmp_file = new File(file_name);
         String real_name = tmp_file.getName();
-        logger.info("upload file -----------------------"+real_name);
+        logger.info("upload file -----------------------" + real_name);
         fileupload.save_upload_file(file.getBytes(), real_name);
+
+
         return "redirect:/file";
     }
 
@@ -109,12 +122,43 @@ public class MainController {
         return "list";
 
     }
-    @ResponseBody
-    @PostMapping(value = "/test")
-    public String get_folder(HttpServletRequest request,Model map) {
 
-        fileupload.upload_fold(request);
-        return "1";
+    @ResponseBody
+    @RequestMapping (value = "/test")
+    public String get_folder(HttpServletRequest request, Model map) {
+
+        HashMap<String, String> a = new HashMap<>();
+        a.put("name", "fengshaomin");
+        for (Map.Entry<String, String> one : a.entrySet()) {
+
+            System.out.println(one.getKey()+one.getValue());
+
+        }
+
+        a.forEach((k,v)->{System.out.println(k+v);});
+
+        ArrayList<String> l = new ArrayList<>();
+        l.add("fengshaomin");
+        l.add("shaomin");
+        l.add("haha");
+        l.add("haha");
+
+
+
+        System.out.println(l);
+
+        System.out.println("--------------------------------------------------------------");
+
+        HashSet s = new HashSet();
+
+        s.addAll(l);
+
+        System.out.println(s);
+        l.forEach(item->{System.out.println(item);});
+        List<String> li = FileUtil.listFileNames(upload_path);
+
+        String ip = NetUtil.getLocalhost().getHostAddress();
+        return Convert.toStr(ip);
 
     }
 
